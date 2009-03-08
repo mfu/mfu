3d7888ac1db18f22bc2832ef16bc31597089a7f7
Florent Valdelievre <florentvaldelievre@gmail.com>
2009-03-08 17:20:58 +0000

    big commit, change dojo location in repository

--- js/nls/messages.js
+++
@@ -1,8 +0,0 @@
-{
-	attachAnotherFile: "Join another image",
-	deleteFile: "Delete",
-	retryFailedUploads: "Retry failed upload(s)",
-	NETWORK_ERROR: "The server cannot be reached. Please retry",
-	SIZE_EXCEEDED: "Size limit exceeded",
-	FORMAT_NOT_RECOGNIZED: "Format cannot be recognized"
-}
